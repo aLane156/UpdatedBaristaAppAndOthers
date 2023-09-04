9435dae44922655be29e9af52a88a79d72e1e60c
Josh Taylor <jt153368@renishaw.com>
2023-09-04 09:04:22 +0100

    added new vertion of to do

---
+++ Josh Taylor/OldToDo/ToDo/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class Task
+    {
+        public string description { get; set; }
+        public bool complete { get; set; }
+        public string date { get; set; }
+    }
+    public class TaskList : Dictionary<string, Task>
+    {
+    }
+    public class TaskSelected
+    {
+        public int id = -1;
+        public string description = null;
+        public bool complete = false;
+        public int date = -1;
+    }
+}
