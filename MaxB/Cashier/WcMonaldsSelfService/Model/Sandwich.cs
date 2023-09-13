23c11ebb17a68ce4bfaaf2ff5fca75d227ace0b8
Max Bramworth <mb153367@renishaw.com>
2023-09-13 15:24:41 +0100

    Adding basket functionality, a total price display, deleting and duplicating items in basket

---
+++ MaxB/Cashier/WcMonaldsSelfService/Model/Sandwich.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcMonaldsSelfService.Model
+{
+    public class LooseMeats : MenuItem
+    {
+        public LooseMeats(string _name, float _price, int _count) : base(_name, _price)
+        {
+
+        }
+    }
+}
