b12d08ec928a51ec8b4f6e6cced0d0e25bfff176
Josh Taylor <jt153368@renishaw.com>
2023-09-13 10:38:14 +0100

    added cafe project

---
+++ Josh Taylor/cafeTill/cafeTill/ViewModels/mainMenuViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cafeTill.ViewModels
+{
+    internal class mainMenuViewModel
+    {
+    }
+}
