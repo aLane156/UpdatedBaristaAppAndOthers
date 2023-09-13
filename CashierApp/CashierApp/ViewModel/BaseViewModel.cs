c59e27a93db5dc60499d6dad5b824dd73315d3bf
Jack Whatley <jw153369@renishaw.com>
2023-09-13 12:11:48 +0100

    Setup MVVM

---
+++ CashierApp/CashierApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierApp.ViewModel
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) return;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CashierApp/CashierAppUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CashierAppUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
